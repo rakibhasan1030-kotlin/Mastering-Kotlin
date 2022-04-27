18c3191b58ae6175b90d1c0391e9322f1ae2dc5c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-27 13:20:45 +0600

    005-StandardLibraryFunction added!

---
+++ src/main/kotlin/function/005-StandardLibraryFunction.kt
@@ -0,0 +1,15 @@
+package function
+
+import kotlin.math.max
+import kotlin.math.sqrt
+
+fun main(){
+    /* Standard Library Function is also known as built-in library function */
+
+    var a : Int = max(25,35)
+    println("MAX between 25 and 35 is = $a")
+
+    var b : Double = sqrt(25.0)
+    println("SquareRoot of 25 is = $b")
+
+}
