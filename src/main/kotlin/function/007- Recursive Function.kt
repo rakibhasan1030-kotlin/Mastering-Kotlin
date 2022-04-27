b73cd19390ab18f858200a10b39e9eb223435640
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-27 15:08:04 +0600

    007- Recursive Function added!

---
+++ src/main/kotlin/function/007- Recursive Function.kt
@@ -0,0 +1,14 @@
+package function
+
+fun main(){
+    var a : Long = 10
+    println(recSum(a, 0))
+}
+
+fun recSum(n : Long, sum : Long) : Long{
+    return if(n < 1){
+        sum;
+    }else{
+        recSum(n-1, n + sum)
+    }
+}
